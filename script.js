580de8929ef4ac13de072cdb80dd86145f50098c
montano-oscar <osc428@gmail.com>
2022-03-05 13:34:36 -0500

    LandingPage

---
+++ script.js
@@ -0,0 +1,30 @@
+//carousel slide animation, when event triggers capture active slide(from) and selected slide(to)
+
+let carruselFcn = document.getElementById("carouselExampleIndicators")
+.addEventListener('slide.bs.carousel',e => slideButtonAnimation(e.from,e.to))
+
+
+function slideButtonAnimation(num1,num2) {
+    //get elements from DOM
+    let slidebtn1 = document.getElementById("slidebtn1")
+    let slidebtn2 = document.getElementById("slidebtn2")
+    let slidebtn3 = document.getElementById("slidebtn3")
+
+    // dispatch tables
+    let fromTable = {
+        0 : () => slidebtn1.classList.remove("slider-selected"),
+        1 : () => slidebtn2.classList.remove("slider-selected"),
+        2 : () => slidebtn3.classList.remove("slider-selected"),
+        undefined
+    }
+    let toTable = {
+        0 : () => slidebtn1.classList.add("slider-selected"),
+        1 : () => slidebtn2.classList.add("slider-selected"),
+        2 : () => slidebtn3.classList.add("slider-selected"),
+        undefined
+    }    
+    //apply styles accordingly
+    fromTable[num1]()
+    toTable[num2]()
+}
+
